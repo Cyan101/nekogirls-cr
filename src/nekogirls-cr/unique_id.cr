f72b057041921b7ccb14e1246599cf91d4060482
Jos Spencer <crazyandroid101@gmail.com>
2017-06-11 22:18:11 +0800

    Some code to generate a unique id for the file

---
+++ src/nekogirls-cr/unique_id.cr
@@ -0,0 +1,16 @@
+Alpha52 = ('a'..'z').to_a + ('A'..'Z').to_a
+
+def to_s52(input)
+  s = ""
+  q = input
+  while q > 0
+    q, r = (q - 1).divmod(52)
+    s = Alpha52[r] + s
+    break if q == 0
+  end
+  return s
+end
+
+def unique_id
+  return to_s52(Time.now.epoch_ms)
+end
