031aff8f7d507b66c3cf4a28ce7b4653b6d9dc0c
Jos Spencer <crazyandroid101@gmail.com>
2017-06-11 23:14:13 +0800

    Removed un-needed code since its already set as a String

--- src/nekogirls.cr
+++ src/nekogirls.cr
@@ -3,6 +3,7 @@ require "kemal"
 
 Kemal.config.port = 8080
 Kemal.config.public_folder = "./src/public/"
+Kemal.config.host_binding = "localhost"
 
 module Nekogirls
   get "/" do
@@ -17,10 +18,6 @@ module Nekogirls
     file = env.params.files["file_to_upload"]
     filename = file.filename.as(String)
     file_ext = File.extname(filename)
-    # Be sure to check if file.filename is not empty otherwise it'll raise a compile time error
-    if !filename.is_a?(String)
-      env.response.status_code = 400
-    else
       newfilename = unique_id + file_ext
       file_path = "./src/public/p/#{newfilename}"
       File.open(file_path, "w") do |f|
